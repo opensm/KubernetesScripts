b460b7ba4773b865767fd1fc4714fef03f796092
opensm <smxblysq@qq.com>
2022-02-18 16:54:14 +0800

    Update copyjenkins.py

--- copyjenkins.py
+++ copyjenkins.py
@@ -3,18 +3,28 @@ import jenkins
 
 class CopyJenkins:
     def __init__(self):
-        self.jenkins = jenkins.Jenkins("http://111.111.111.111:8080/", username='admin', password='1q2w3e4r')
-        self.jenkins_new = jenkins.Jenkins("http://111.111.111.112:8080/", username='admin', password='xxx@2019')
+        self.jenkins = jenkins.Jenkins("http://127.0.0.1:8080/", username='admin', password='111?')
+        self.jenkins_new = jenkins.Jenkins("http://127.0.0.1:8080/", username='admin', password='111?')
 
     def copy_jenkins(self):
         """
         :return:
         """
-        for name in [
-            'xxx', 'xxxy'
-        ]:
-            result = self.jenkins.get_job_config(name=name)
-            self.jenkins_new.create_job(name=name, config_xml=result)
+        data = self.jenkins.get_jobs(view_name='统一车联网平台-开发')
+        for task in data:
+            result = self.jenkins.get_job_config(name=task['name'])
+            print(result.replace(
+                    'env.sh',
+                    'env_test.sh'
+                ))
+
+            self.jenkins_new.create_job(
+                name="{}-test".format(task['name']),
+                config_xml=result.replace(
+                    'env.sh',
+                    'env_test.sh'
+                )
+            )
 
 
 c = CopyJenkins()
